e8abdbc57a9086ee07bad0077c9c68eeed6522f3
Ahmet Güler <l1517024@std.yildiz.edu.tr>
2021-07-23 15:21:18 +0300

    testing 23/07/2021

--- Notes.py
+++ Notes.py
@@ -1,7 +1,7 @@
 
 """
 
-git add . && git status && git commit -m "migration 23/07/2021"
+git add . && git status && git commit -m "testing 23/07/2021"
 git checkout main && git pull
 
 pip list --format freeze > requirements.txt
