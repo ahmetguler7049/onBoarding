ced96c1198587c8ce9a910fcd8df27843dc272e9
Ahmet Güler <l1517024@std.yildiz.edu.tr>
2021-07-25 20:53:56 +0300

    subdomain_middleware 25/07/2021

--- onBoarding/settings.py
+++ onBoarding/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
-    # 'onBoarding.SubdomainMiddleware.SubdomainMiddleware',
+    'onBoarding.SubdomainMiddleware.SubdomainMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
